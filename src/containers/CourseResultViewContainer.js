bd03afa5c8931fa7d413ed6b8b23e81fe7047fc6
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-09 01:49:03 +0800

    Added components to course result panel.

--- src/containers/CourseResultViewContainer.js
+++ src/containers/CourseResultViewContainer.js
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import {
     Box,
     Flex,
@@ -8,36 +8,63 @@ import {
     Tab,
     TabPanels,
     TabPanel,
+    Switch,
+    FormControl,
+    FormLabel,
+    Collapse,
+    IconButton,
+    Container,
 } from '@chakra-ui/react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import CourseInfoRowContainer from './CourseInfoRowContainer';
 import DataSet from '../components/FakeDataSet';
+import CourseSearchInput from '../components/CourseSearchInput';
 
 
 function CourseResultViewContainer() {
+    const [ displayFilter, setDisplayFilter ] = useState(false);
+    const renderSettingSwitch = (label, default_checked) => {
+        return(
+            <FormControl display='flex' alignItems='center' my="2">
+                <Switch id='add-nol' defaultChecked={default_checked} mr="2"/>
+                <FormLabel htmlFor='add-nol' mb='0' fontWeight="500" color="gray.600">
+                    {label}
+                </FormLabel>
+            </FormControl>
+        );
+    };
     return (
-        <Box maxW="screen-md" mx="auto" overflow="visible" px="64px" pt="64px" pb="40px">
+        <Box display='flex'flexDirection="column" alignItems='center' h="95vh" overflow="auto" maxW="screen-md" mx="auto" pt="64px" pb="40px">
+            <Flex w="100%" direction="column" position="sticky" top="0" bgColor="white" zIndex="100" boxShadow="md">
+                <Flex w="100%" px="20vw" py="4" direction="column" >
+                    <CourseSearchInput />
+                    <Collapse in={displayFilter}>
+                        <Box w="100%" py="8px" mt="4">
+                            <Tabs>
+                                <TabList>
+                                    <Tab><Text color="gray.700" fontSize="xl" fontWeight="700">篩選</Text></Tab>
+                                    <Tab><Text color="gray.700" fontSize="xl" fontWeight="700">設定</Text></Tab>
+                                </TabList>
+                                <TabPanels>
+                                    <TabPanel>
+                                    讚
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <Flex flexDirection="column" alignItems="center">
+                                            {renderSettingSwitch('顯示已選課程', true)}
+                                            {renderSettingSwitch('只顯示未衝堂課程', false)}
+                                            {renderSettingSwitch('同步新增至課程網', false)}
+                                        </Flex>
+                                    </TabPanel>
+                                </TabPanels>
+                            </Tabs>
+                        </Box>
+                    </Collapse>
+                </Flex>
+                <IconButton size="sm" variant='ghost' icon={displayFilter? <FaChevronUp />:<FaChevronDown />} onClick={() => setDisplayFilter(!displayFilter)} />
+            </Flex>
             <Flex direction="row" justifyContent="center" w="100%">
-                <Flex direction="column">
-                    <Box w="60vw" px="16px" py="16px" my="4" bgColor="blue.50" borderRadius="md">
-                        <Tabs>
-                            <TabList>
-                                <Tab><Text color="gray.700" fontSize="xl" fontWeight="700">篩選</Text></Tab>
-                                <Tab><Text color="gray.700" fontSize="xl" fontWeight="700">排序</Text></Tab>
-                                <Tab><Text color="gray.700" fontSize="xl" fontWeight="700">顯示設定</Text></Tab>
-                            </TabList>
-                            <TabPanels>
-                                <TabPanel>
-                                讚
-                                </TabPanel>
-                                <TabPanel>
-                                讚讚
-                                </TabPanel>
-                                <TabPanel>
-                                讚讚讚
-                                </TabPanel>
-                            </TabPanels>
-                        </Tabs>
-                    </Box>
+                <Flex direction="column" overflow="a">
                     <CourseInfoRowContainer flex='3' courseInfo={DataSet.courseInfo} />
                 </Flex>
             </Flex>
