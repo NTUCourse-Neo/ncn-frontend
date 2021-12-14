ce8d3a4181d48e356d1d1b1cbd916bab4629d3f5
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-14 22:47:32 +0800

    Added course_type data and filter modal panel.

--- src/components/FilterModal.js
+++ src/components/FilterModal.js
@@ -13,47 +13,66 @@ import {
   Divider,
   Flex,
 } from "@chakra-ui/react";
-import { render } from "@testing-library/react";
-import { useState } from "react";
 import { college_map } from '../data/college';
 import { dept_list } from '../data/department';
+import { type_list, code_map } from '../data/course_type';
 
 function FilterModal(props){
-  const renderSelectedDepts = () => {
+  const renderSelectedHeader = () => {
     if (props.type === "department"){
       return(
         <Flex flexDirection="column" justifyContent="start" alignItems="start" mx="8" mb="4">
         <Flex w="100%" flexWrap="wrap" flexDirection="row" justifyContent="start" alignItems="start">
-          {dept_list.map(dept => renderButton(dept, true))} 
+          {dept_list.map(dept => renderButton("department", dept, props.selectedDept, props.setSelectedDept, true))} 
         </Flex>
       </Flex>
       );
     }
+    if (props.type === "category"){
+      return(
+        <Flex flexDirection="column" justifyContent="start" alignItems="start" mx="8" mb="4">
+        <Flex w="100%" flexWrap="wrap" flexDirection="row" justifyContent="start" alignItems="start">
+          {type_list.map(category => renderButton("category", category, props.selectedType, props.setSelectedType, true))} 
+        </Flex>
+        </Flex>
+      );
+    }
     return (<></>);
   };
-  const handleSelectDept = (value) => {
-    var index = props.selectedDept.indexOf(value);
+  const handleSelect = (value, selected, setSelected) => {
+    var index = selected.indexOf(value);
         if (index === -1) {
-          props.setSelectedDept([...props.selectedDept, value]);
+          setSelected([...selected, value]);
         } else {
-          props.selectedDept.splice(index, 1);
-          props.setSelectedDept([...props.selectedDept]);
+          selected.splice(index, 1);
+          setSelected([...selected]);
         }
   };
-  const renderButton = (dept, renderSelected) => {
-    var index = props.selectedDept.indexOf(dept.code);
+  const renderButton = (type, data, selected, setSelected, renderSelected) => {
+    var key;
+    switch(type){
+      case "department":
+        key = data.code
+        break;
+      case "category":
+        key = data.id
+        break;
+      default:
+        key = data.code
+    }
+    var index = selected.indexOf(key);
     if((index === -1) === !renderSelected){
       return(
-        <Button key={dept.code} 
+        <Button key={key} 
                 colorScheme="teal" 
                 variant={index === -1 ? "outline":"solid"} 
                 size="sm" 
                 minW="100px" 
                 m="1"
-                onClick={()=>handleSelectDept(dept.code)}
+                onClick={()=>handleSelect(key, selected, setSelected)}
                 _hover={index === -1 ? { bg: "teal.100" }:{ bg: "red.700" }}>
-          <Badge mx="2" colorScheme="blue">{dept.code}</Badge>
-          {dept.full_name}
+          <Badge mx="2" colorScheme="blue">{data.code}</Badge>
+          {data.full_name}
         </Button>
       );
     }else{
@@ -62,6 +81,15 @@ function FilterModal(props){
   };
   const FilterModalContainer = (title, filterOn, type) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    var setSelected;
+    switch(type){
+      case "department":
+        setSelected = props.setSelectedDept;
+        break;
+      case "category":
+        setSelected = props.setSelectedType;
+        break;
+    }
     return (
     <>
     <Button isDisabled={!filterOn} onClick={onOpen}>{title}</Button>
@@ -74,7 +102,7 @@ function FilterModal(props){
           </Flex>
         </ModalHeader>
         <ModalCloseButton />
-        {renderSelectedDepts()}
+        {renderSelectedHeader()}
         <ModalBody overflow="auto" pt="0">
           {FilterModalBody(type)}
         </ModalBody>
@@ -82,7 +110,7 @@ function FilterModal(props){
           <Button colorScheme='blue' mr={3} onClick={onClose}>
             套用
           </Button>
-          <Button variant='ghost' onClick={()=> props.setSelectedDept([])}>重設</Button>
+          <Button variant='ghost' onClick={()=> setSelected([])}>重設</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
@@ -103,12 +131,34 @@ const FilterModalBody = (type) => {
                   <Heading fontSize="2xl" color="gray.600">{college_code+" "+college_map[college_code].name}</Heading>
                   <Divider/>
                 </Flex>
-                {renderButton(dept, false)}
+                {renderButton("department", dept, props.selectedDept, props.setSelectedDept, false)}
                 </>
               );
             }
             return(
-              renderButton(dept, false)
+              renderButton("department", dept, props.selectedDept, props.setSelectedDept, false)
+            );
+          })}
+        </>
+      );
+    }
+    if (type === "category"){
+      return(
+        <>
+          {type_list.map((type, index) => {
+            if (index === 0 || type.code.substr(0,1) !== type_list[index-1].code.substr(0,1)){
+              return(
+                <>
+                <Flex px="2" h="40px" flexDirection="column" justifyContent="center" position="sticky" top="0" mt={index === 0 ? "0":"6"} bgColor="white" zIndex="50">
+                  <Heading fontSize="2xl" color="gray.600">{code_map[type.code.substr(0,1)].name}</Heading>
+                  <Divider/>
+                </Flex>
+                {renderButton("category", type, props.selectedType, props.setSelectedType, false)}
+                </>
+              );
+            }
+            return(
+              renderButton("category", type, props.selectedType, props.setSelectedType, false)
             );
           })}
         </>
--- src/containers/CourseResultViewContainer.js
+++ src/containers/CourseResultViewContainer.js
@@ -30,6 +30,8 @@ function CourseResultViewContainer() {
   const search_results = useSelector(state => state.search_results);
   const search_settings = useSelector(state => state.search_settings);
   const [selectedDept, setSelectedDept] = useState([]);
+  const [selectedType, setSelectedType] = useState([]);
+  const [selectedTime, setSelectedTime] = useState([]);
 
   const [ timeFilterOn, setTimeFilterOn ] = useState(false);
   const [ deptFilterOn, setDeptFilterOn ] = useState(false);
@@ -88,7 +90,7 @@ function CourseResultViewContainer() {
                                                     <Switch size="lg" mr="2" onChange={ (e) => {
                                                       setTimeFilterOn(e.currentTarget.checked);
                                                     } }/>
-                                                      <FilterModal title="未選擇課程時間" toggle={timeFilterOn} type="time" selectedDept={selectedDept} setSelectedDept={setSelectedDept}/>
+                                                      <FilterModal title="未選擇課程時間" toggle={timeFilterOn} type="time" selectedTime={selectedTime} setSelectedTime={setSelectedTime}/>
                                                     </Flex>
                                                 </Flex>
                                                 <Flex flexDirection="column" w="30%" px="4">
@@ -104,7 +106,7 @@ function CourseResultViewContainer() {
                                                     <Switch size="lg" mr="2" onChange={ (e) => {
                                                       setCatFilterOn(e.currentTarget.checked);
                                                     } }/>
-                                                    <FilterModal title="未選擇課程類別" toggle={catFilterOn} type="category" selectedDept={selectedDept} setSelectedDept={setSelectedDept}/>
+                                                    <FilterModal title={selectedType.length===0 ? "未選擇課程類別" : "已選擇 "+selectedType.length+" 類別"} toggle={catFilterOn} type="category" selectedType={selectedType} setSelectedType={setSelectedType}/>
                                                     </Flex>
                                                 </Flex>
                                                 
