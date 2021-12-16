89314f15b23bb8c1f70b4b65447aa05365819f42
b07502089 <a6140000@gmail.com>
2021-12-16 22:14:23 +0800

    test bottom detecter

---
+++ src/hooks/useOnScreen.js
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react'
+
+export default function useOnScreen(ref) {
+
+    const [isIntersecting, setIntersecting] = useState(false)
+  
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+  
+    useEffect(() => {
+      observer.observe(ref.current)
+      // Remove the observer as soon as the component is unmounted
+      return () => { observer.disconnect() }
+    }, [])
+  
+    return isIntersecting
+}
