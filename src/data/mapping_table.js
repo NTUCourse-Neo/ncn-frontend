560535175fbbd04dfe789c6d34a11a5a4c483892
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-21 14:45:04 +0800

    Added mapping table.

--- src/data/mapping_table.js
+++ src/data/mapping_table.js
@@ -1,10 +1,21 @@
+import { FaUserPlus, FaPuzzlePiece } from 'react-icons/fa';
 const mapping_tables = {
-    requirements: {
-        "0": "必帶",
-        "1": "必修",
-        "2": "選修",
-        "3": "其他"
-    }
+    required: {
+        "name": "必選修",
+        "logo": FaPuzzlePiece,
+        "color": "blue",
+        "map":{
+            "0": "必帶",
+            "1": "必修",
+            "2": "選修",
+            "3": "其他"
+        }
+    },
+    total_slot: {
+        "name": "修課人數上限",
+        "logo": FaUserPlus,
+        "color": "blue"
+    },
 }
 
 export default mapping_tables;
