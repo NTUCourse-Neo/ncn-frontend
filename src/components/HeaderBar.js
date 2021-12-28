618b82fc05a2670bd40bf3b5d05446b15957235d
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-28 17:57:34 +0800

    Added user icon.

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -4,13 +4,18 @@ import {
   Heading,
   Button,
   Spacer,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Kbd,
+  Avatar,
+  AvatarBadge,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuGroup,
+  MenuItem,
+  MenuDivider,
+
 } from '@chakra-ui/react';
 import {ChevronRightIcon, Search2Icon} from "@chakra-ui/icons"
+import { FaCheck, FaExclamation } from 'react-icons/fa';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -23,7 +28,33 @@ function HeaderBar() {
       return <Button colorScheme="blue" variant="ghost" size="md" ml="10px" mr="10px" isLoading />
     }
     if(isAuthenticated) {
-      return (<Button colorScheme="yellow" variant="outline" size="md" ml="10px" mr="10px" onClick={() => {logout()}}>{user.name}</Button>);
+      return (
+        <>
+          <Menu>
+            <MenuButton as={Avatar} name={user.name} src={user.picture}>
+              {
+                user.email_verified ?
+                <AvatarBadge boxSize='1.25em' bg='green.500'><FaCheck size="10"/></AvatarBadge>:
+                <AvatarBadge boxSize='1.25em' bg='orange.500'><FaExclamation size="10"/></AvatarBadge>
+              }
+            </MenuButton>
+            <MenuList>
+              <MenuGroup title='帳戶'>
+                <MenuItem>個人資料</MenuItem>
+                <MenuItem>設定</MenuItem>
+              </MenuGroup>
+              <MenuDivider />
+              <MenuGroup title='更多'>
+                <MenuItem>常見問題</MenuItem>
+                <MenuItem>關於</MenuItem>
+              </MenuGroup>
+              <Flex justifyContent="end" alignItems="center">
+                <Button colorScheme="red" variant="solid" size="md" m="2" onClick={() => logout()}>登出</Button>
+              </Flex>
+            </MenuList>
+          </Menu>
+        </>
+      );
     }
     return(
       <>
