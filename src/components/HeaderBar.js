a12228751392fbafff3afe0849175e915da81d1f
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-28 17:38:51 +0800

    Added auth0 provider and updated build script.

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -13,21 +13,35 @@ import {
 import {ChevronRightIcon, Search2Icon} from "@chakra-ui/icons"
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 function HeaderBar() {
-    return (
-      <Flex position="fixed" w="100%" h="64px" bg="teal.300" flexDirection="row" justifyContent="start" alignItems="center" zIndex="1000">
-        <Flex justifyContent="center" alignItems="center" ml="60px">
-          <Link to="/"><Heading fontSize="2xl" fontWeight="700" mr="auto" color="gray.700" minW="200px">NTUCourse Neo</Heading></Link>
-          <Link to="/course"><Button colorScheme="blue" variant="ghost" size="md" ml="30px">課程</Button></Link>
-        </Flex>
-        <Spacer />
-        <Flex justifyContent="center" alignItems="center" mr="60px">
-          <Link to="/login"><Button colorScheme="blue" variant="ghost" size="md" ml="10px" mr="10px">登入</Button></Link>
-          <Link to="/signup"><Button colorScheme="yellow" rightIcon={<ChevronRightIcon/>} size="md" ml="10px" mr="10px">註冊</Button></Link>
-        </Flex>
+  const { loginWithRedirect, user, isAuthenticated, isLoading, logout  }  = useAuth0();
+  const renderSignInButton = () => {
+    if(isLoading) {
+      return <Button colorScheme="blue" variant="ghost" size="md" ml="10px" mr="10px" isLoading />
+    }
+    if(isAuthenticated) {
+      return (<Button colorScheme="yellow" variant="outline" size="md" ml="10px" mr="10px" onClick={() => {logout()}}>{user.name}</Button>);
+    }
+    return(
+      <>
+        <Button colorScheme="yellow" rightIcon={<ChevronRightIcon/>} size="md" ml="10px" mr="10px" onClick={() => loginWithRedirect()}>登入 / 註冊</Button>
+      </>
+    );
+  };
+  return (
+    <Flex position="fixed" w="100%" h="64px" bg="teal.300" flexDirection="row" justifyContent="start" alignItems="center" zIndex="1000">
+      <Flex justifyContent="center" alignItems="center" ml="60px">
+        <Link to="/"><Heading fontSize="2xl" fontWeight="700" mr="auto" color="gray.700" minW="200px">NTUCourse Neo</Heading></Link>
+        <Link to="/course"><Button colorScheme="blue" variant="ghost" size="md" ml="30px">課程</Button></Link>
       </Flex>
+      <Spacer />
+      <Flex justifyContent="center" alignItems="center" mr="60px">
+        {renderSignInButton()}
+      </Flex>
+    </Flex>
   );
 }
 
