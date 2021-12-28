de7afbfc19b7c9c21bced57976c62461b7b26f9f
jc-hiroto <taste.ful.msa@gmail.com>
2021-12-28 20:59:29 +0800

    Refined user menu.

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -12,6 +12,8 @@ import {
   MenuGroup,
   MenuItem,
   MenuDivider,
+  Text,
+  Badge
 
 } from '@chakra-ui/react';
 import {ChevronRightIcon, Search2Icon} from "@chakra-ui/icons"
@@ -19,19 +21,21 @@ import { FaCheck, FaExclamation } from 'react-icons/fa';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import BeatLoader from 'react-spinners/BeatLoader';
 
 
 function HeaderBar() {
   const { loginWithRedirect, user, isAuthenticated, isLoading, logout  }  = useAuth0();
   const renderSignInButton = () => {
     if(isLoading) {
-      return <Button colorScheme="blue" variant="ghost" size="md" ml="10px" mr="10px" isLoading />
+      return <Button colorScheme="blue" variant="ghost" size="md" ml="10px" mr="10px" spinner={<BeatLoader size={8} color='white' />} isLoading />
     }
     if(isAuthenticated) {
       return (
         <>
+          <Button colorScheme="blue" variant="ghost" size="md" mx="2" mr="4">我的</Button>
           <Menu>
-            <MenuButton as={Avatar} name={user.name} src={user.picture}>
+            <MenuButton as={Avatar} name={user.name} src={user.picture} _hover={{cursor:"pointer"}}>
               {
                 user.email_verified ?
                 <AvatarBadge boxSize='1.25em' bg='green.500'><FaCheck size="10"/></AvatarBadge>:
@@ -41,7 +45,6 @@ function HeaderBar() {
             <MenuList>
               <MenuGroup title='帳戶'>
                 <MenuItem>個人資料</MenuItem>
-                <MenuItem>設定</MenuItem>
               </MenuGroup>
               <MenuDivider />
               <MenuGroup title='更多'>
@@ -49,7 +52,12 @@ function HeaderBar() {
                 <MenuItem>關於</MenuItem>
               </MenuGroup>
               <Flex justifyContent="end" alignItems="center">
-                <Button colorScheme="red" variant="solid" size="md" m="2" onClick={() => logout()}>登出</Button>
+                <Flex flexDirection="column" justifyContent="center" alignItems="start" m="2" ml="4">
+                  <Badge colorScheme={user.email_verified ? "blue":"yellow"} mb="1">{user.email_verified ? "臺大學生":"未驗證"}</Badge>
+                  <Text fontSize="sm" color="gray.600" fontWeight="700">{user.name}</Text>
+                  <Text fontSize="xs" color="gray.500" fontWeight="500">{user.email}</Text>
+                </Flex>
+                <Button colorScheme="red" variant="outline" size="md" m="2" mr="4" onClick={() => logout()}>登出</Button>
               </Flex>
             </MenuList>
           </Menu>
