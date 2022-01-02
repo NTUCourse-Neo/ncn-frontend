4fb3babd57ecdd1e4ccc95a183a57586fa838788
jc-hiroto <taste.ful.msa@gmail.com>
2022-01-02 15:54:47 +0800

    Updated user info page.

--- src/containers/UserInfoContainer.js
+++ src/containers/UserInfoContainer.js
@@ -3,12 +3,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 import {
     Box,
     Flex,
+    Spacer,
     Text
 } from '@chakra-ui/react';
 import { get_user_by_id } from '../api/users';
+import LoadingOverlay from 'react-loading-overlay';
+import { HashLoader } from 'react-spinners';
 function UserInfoContainer(props) {
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
   const { user, isLoading }  = useAuth0();
+  const userLoading = isLoading || !userInfo;
   useEffect(async() => {
     if(!isLoading && user) {
       console.log(user);
@@ -21,10 +25,22 @@ function UserInfoContainer(props) {
     }
   }, [user]);
 
-  return (
-    <Box maxW="screen-md" minH="95vh" mx="auto" overflow="visible" p="64px">
+  if(userLoading) {
+    return(
+      <Box maxW="screen-md" h="95vh" mx="auto" overflow="visible" p="64px">
+        <Flex h="90vh" justifyContent="center" flexDirection="column" alignItems="center">
+          <HashLoader size="60px" color="teal"/>
+        </Flex>
+      </Box>
+    );
+  }
+  return(
+    <Box maxW="screen-md" h="95vh" mx="auto" overflow="visible" p="64px">
       <Flex justifyContent="space-between" mb={4} grow="1" flexDirection="column" alignItems="center">
-        <Text>歡迎回來，{userInfo.db.name}</Text>
+        <Text fontSize="3xl" fontWeight="700" color="gray.600" my="4">✌️ 歡迎回來，{userInfo.db.name}</Text>
+        <Flex h="80vh" w="100%" flexDirection="column" justifyContent="start" alignItems="center" p="4" bg="gray.100" borderRadius="xl" boxShadow="lg">
+
+        </Flex>
       </Flex>
     </Box>
   );
