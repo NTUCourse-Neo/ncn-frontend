290aa4c85d7945b044cdf75c40800c4dc363bfa2
b07502089 <a6140000@gmail.com>
2022-01-12 18:01:20 +0800

    add random error page display msg

--- src/containers/ErrorContainer.js
+++ src/containers/ErrorContainer.js
@@ -9,9 +9,9 @@ import {
 function ErrorContainer(props){
 
     const error_img_src = `https://http.cat/${props.code}`
-    // can random pick one msg from the list
-    const error_msgs = [`太無情了你真的太無情了`, `出事了阿北`, `==?`] 
-    const error_message = `太無情了你真的太無情了`
+    // can random pick one msg from the list, welcome to add more msgs
+    const error_msgs = [`太無情了你真的太無情了`, `出事了阿北`, `==?`, `哭啊`] 
+    const error_message = error_msgs[Math.floor(Math.random() * error_msgs.length)];
 
     return (
         <Center flexDirection='column' justifyItems="center" maxW="60vw" mx="auto" overflow="visible" p="64px" h='95vh' >
@@ -19,6 +19,7 @@ function ErrorContainer(props){
                 <a href="https://youtu.be/yKrR5IHwT0k" target="_blank" rel="noreferrer noopener">
                     <Text fontSize='4xl' color='gray.500' mt='2vh'>{error_message}</Text>
                 </a>
+                {/* replace by link of bug report form ? */}
                 <Text fontSize='xl' color='gray.500' mt='0.5vh'>(For dev only) Open your console to see error &#11015; &#128064;</Text>
         </Center>
     )
