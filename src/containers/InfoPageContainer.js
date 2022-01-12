dc5b9f0a65009148a4f0cbdfda6cb1bc2a4ec7e4
jc-hiroto <taste.ful.msa@gmail.com>
2022-01-13 02:42:10 +0800

    Added a liver.

--- src/containers/InfoPageContainer.js
+++ src/containers/InfoPageContainer.js
@@ -10,10 +10,12 @@ import {
     VStack,
     Image,
     Icon,
-    Button
+    Button,
+    Tooltip
   } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
 import { SiNotion, SiDiscord } from 'react-icons/si';
+import {GiLiver} from 'react-icons/gi';
 
 const teams = [
   {
@@ -86,7 +88,7 @@ function InfoPageContainer(props){
         </Flex>
         <Text fontSize="5xl" fontWeight="800" color="gray.700">Powered by...</Text>
         <Divider />
-        <HStack alignItems="center" p="16" spacing="8">
+        <HStack alignItems="center" justifyContent="center" p="16" spacing="8" wrap="wrap" w="90vw" css={{ gap: '2em' }}>
           <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Microsoft_Azure_Logo.svg/1200px-Microsoft_Azure_Logo.svg.png" height="50px" />
           <Image src="https://res.cloudinary.com/citiar/image/upload/v1611373461/ucamc/common/react_icon.png" height="50px" />
           <Image src="https://cg2010studio.files.wordpress.com/2016/07/nodejs.png?w=584" height="50px" />
@@ -97,12 +99,17 @@ function InfoPageContainer(props){
           <Image src="https://miro.medium.com/max/1200/0*4L-GqAxAp7KsObMj.png" height="50px" />
           <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/RecaptchaLogo.svg/1200px-RecaptchaLogo.svg.png" height="50px" />
         </HStack>
-        <HStack spacing={4}>
-          <Text fontSize="2xl" fontWeight="500" color="gray.700">And with the help of...</Text>
+        <HStack spacing={4} mb="4">
+          <Text fontSize="2xl" fontWeight="500" color="gray.700">with the help of...</Text>
           <Icon as={FaGithub} w="8" h="8" color="gray.700"/>
           <Icon as={SiDiscord} w="8" h="8" color="gray.700"/>
           <Icon as={SiNotion} w="8" h="8" color="gray.700"/>
-        <Text fontSize="2xl" fontWeight="500" color="gray.700">and our 🔥 and 💖 !</Text>
+        <Text fontSize="2xl" fontWeight="500" color="gray.700">and our 🔥 and 💖 ! ... and don't forget our</Text>
+        <Tooltip hasArrow placement="top" label="肝 ㄍㄢ Liver" bg='gray.600' color='white'>
+          <span>
+            <Icon as={GiLiver} w="8" h="8" color="red.500" pt="2"/>
+          </span>
+        </Tooltip>
         </HStack>
       </Flex>
     </>
