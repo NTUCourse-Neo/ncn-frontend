69255ce78bd04782f7e2196d1d1e82a8f948efbc
b07502089 <a6140000@gmail.com>
2022-01-17 21:31:07 +0800

    block direct navigate to error page

--- src/containers/ErrorContainer.js
+++ src/containers/ErrorContainer.js
@@ -6,10 +6,12 @@ import {
     HStack
 } from '@chakra-ui/react';
 import { FaHeartbeat } from 'react-icons/fa';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function ErrorContainer(props){
     const error_page_states = useLocation().state;
+    const navigate = useNavigate();
 
     const handleOpenPage = (page) => {
         window.open(page, '_blank');
@@ -19,21 +21,13 @@ function ErrorContainer(props){
     const error_msgs = [`太無情了你真的太無情了`, `出事了阿北`, `==?`, `哭啊`] 
     const error_message = error_msgs[Math.floor(Math.random() * error_msgs.length)];
 
-    console.log(error_page_states);
-    if (!error_page_states){
-        console.log('NOT VALID');
-        return (
-        <Center flexDirection='column' justifyItems="center" maxW="60vw" mx="auto" overflow="visible" p="64px" h='95vh' >
-            <Image mt="5vh" src={error_img_src} w="auto" h="80%"/>
-            <a href="https://youtu.be/yKrR5IHwT0k" target="_blank" rel="noreferrer noopener">
-                <Text fontSize='4xl' color='gray.500' mt='2vh'>不可以直接用url打我ㄛ</Text>
-            </a>
-            <HStack spacing={2} mt="4">
-                <Button variant="solid" onClick={() => handleOpenPage("https://www.surveycake.com/s/LzWd6")}>點我回報問題 🥺</Button>
-                <Button variant="solid" colorScheme="teal" leftIcon={<FaHeartbeat />} onClick={() => handleOpenPage("https://status.course.myntu.me/")}>服務狀態</Button>
-            </HStack>
-        </Center>)
-    }
+    // console.log(error_page_states);
+    useEffect(() => {
+        if (!error_page_states){
+            // console.log('NOT VALID');
+            navigate(`/`);
+        }
+    } , []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Center flexDirection='column' justifyItems="center" maxW="60vw" mx="auto" overflow="visible" p="64px" h='95vh' >
