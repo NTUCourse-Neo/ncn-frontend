8a0f73ed0960e35ab5fdb26337797db2aa47056e
jc-hiroto <taste.ful.msa@gmail.com>
2022-01-19 03:11:03 +0800

    Added login alert when creating new table.

--- src/containers/SideCourseTableContainer.js
+++ src/containers/SideCourseTableContainer.js
@@ -39,7 +39,7 @@ import {
     FaRegMeh,
     FaPlusSquare,
     FaPlus,
-    FaTrash
+    FaTrash,
 } from 'react-icons/fa';
 import CourseTableContainer from './CourseTableContainer';
 import { fetchCourseTableCoursesByIds, createCourseTable, linkCoursetableToUser, fetchCourseTable, patchCourseTable, fetchUserById, logIn, updateCourseTable } from '../actions/index';
@@ -55,7 +55,7 @@ const LOCAL_STORAGE_KEY = 'NTU_CourseNeo_Course_Table_Key';
 
 function SideCourseTableContainer(props) {
     const navigate = useNavigate();
-    const {user, isLoading, getAccessTokenSilently} = useAuth0();
+    const {user, isLoading, isAuthenticated, getAccessTokenSilently} = useAuth0();
     const toast = useToast();
     const dispatch = useDispatch();
     const courseTable = useSelector(state => state.course_table);
@@ -75,6 +75,7 @@ function SideCourseTableContainer(props) {
     // TODO: Move this part to a new dedicated component.
     const [isDeletingCourse, setIsDeletingCourse] = useState("");
     console.error = () => {};
+
 
     const [isMobile] = useMediaQuery('(max-width: 760px)');
 
@@ -377,7 +378,6 @@ function SideCourseTableContainer(props) {
     };
     const renderSideCourseTableContent = () => {
       if((courseTable===null || expired===true) && !(loading || isLoading)){
-        // console.log("courseTable is null");
         return(
           <Flex flexDirection="column" justifyContent="center" alignItems="center" h="100%" w="100%">
             <Flex flexDirection="row" justifyContent="center" alignItems="center">
@@ -386,7 +386,13 @@ function SideCourseTableContainer(props) {
               <FaRegHandPointDown size="3vh" style={{color:"gray"}}/>
             </Flex>
             <Text fontSize="2xl" fontWeight="bold" color="gray">{expired?"您的課表已過期":"尚無課表"}</Text>
-            <Button colorScheme="teal" leftIcon={<FaPlusSquare />} onClick={()=>{handleCreateTable()}}>新增課表</Button>
+            <Button colorScheme="teal" leftIcon={<FaPlusSquare />} onClick={()=>{
+              if(isAuthenticated || props.agreeToCreateTableWithoutLogin){
+                handleCreateTable();
+              }else{
+                props.setIsLoginWarningOpen(true);
+              }
+            }}>新增課表</Button>
           </Flex>
         );
       }
@@ -477,7 +483,7 @@ function SideCourseTableContainer(props) {
                     </TabList>
                   </Flex>:
                     <SkeletonText width="15vw" mt="2" h="2" noOfLines={3}/>
-                }
+                  }
             </Flex>
               <TabPanels>
                 <TabPanel>
