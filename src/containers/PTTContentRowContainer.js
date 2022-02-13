294524021ba7d2064ea9b0235449422b764f2def
swh00tw <a6140000@gmail.com>
2022-02-13 22:53:09 +0800

    clean unique warning

--- src/containers/PTTContentRowContainer.js
+++ src/containers/PTTContentRowContainer.js
@@ -113,12 +113,12 @@ const mock = {
 function PTTContentRowContainer({ info, height}){
   const RenderRow = (data) => {
     return(
-      <Flex as="button" px="2" my="1" key={data.id} w="100%" h="10" bg="blue.50" justifyContent="start" alignItems="center" borderRadius="lg" onClick={() => window.open(data.url,"_blank")}>
-        <Badge mr="1" colorScheme="blue">{data.date}</Badge>
-        <Text w="50%" mr="2" fontSize="md" color="gray.700" fontWeight="500" textAlign="start" isTruncated>{data.title}</Text>
-        <Text w="20%" as="i" fontSize="xs" color="gray.400" fontWeight="500" textAlign="start" isTruncated>- {data.author}</Text>
+      <Flex key={data.title} as="button" px="2" my="1" w="100%" h="10" bg="blue.50" justifyContent="start" alignItems="center" borderRadius="lg" onClick={() => window.open(data.url,"_blank")}>
+        <Badge key={data.title+"Badge"} mr="1" colorScheme="blue">{data.date}</Badge>
+        <Text key={data.title+"Title"} w="50%" mr="2" fontSize="md" color="gray.700" fontWeight="500" textAlign="start" isTruncated>{data.title}</Text>
+        <Text key={data.title+"Author"} w="20%" as="i" fontSize="xs" color="gray.400" fontWeight="500" textAlign="start" isTruncated>- {data.author}</Text>
         <Spacer />
-        <Icon as={IoMdOpen} color="gray.400" size="20px" />
+        <Icon key={data.title+"Icon"} as={IoMdOpen} color="gray.400" size="20px" />
       </Flex>
     );
   };
