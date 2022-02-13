ecb4ad1c01f0a4dcdf7b766369554c445b4eb27d
jc-hiroto <taste.ful.msa@gmail.com>
2022-02-13 14:54:48 +0800

    Added actions for social API.

--- src/actions/index.js
+++ src/actions/index.js
@@ -907,6 +907,263 @@ const use_otp_link_student_id = (token, student_id, otp_code) => async (dispatch
     return resp.data;
 };
 
+const getSocialPostByCourseId = (token, course_id) => async (dispatch)=>{
+    try {
+        const {data: {posts}} = await instance.get(`/social/courses/${course_id}/posts`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return posts
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
+const getSocialPostByPostId = (token, post_id) => async (dispatch)=>{
+    try {
+        const {data: {post}} = await instance.get(`/social/posts/${post_id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return post
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
+const createSocialPost = (token, course_id, post) => async (dispatch)=>{
+    try {
+        await instance.post(`/social/courses/${course_id}/posts`, {
+            post: post,
+            // includes: content, post_type, user_type
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
+const reportSocialPost = (token, post_id, report) => async (dispatch)=>{
+    try {
+        await instance.post(`/social/posts/${post_id}/report`, {
+            report: report,
+            // includes: content, post_type, user_type
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
+const voteSocialPost = (token, post_id, type) => async (dispatch)=>{
+    try {
+        await instance.patch(`/social/posts/${post_id}/votes`, {
+            type: type,
+            // includes: content, post_type, user_type
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
+const deleteSocialPost = (token, post_id) => async (dispatch)=>{
+    try {
+        await instance.delete(`/social/posts/${post_id}/`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    } catch (error) {
+        if (error.response) {
+            // server did response, used for handle custom error msg
+            let error_obj = {
+                status_code: error.response.status, 
+                backend_msg: error.response.data.message, 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else if (error.request) {
+            // The request was made but no response was received (server is downed)
+            let status = 521; // Server is down
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            let status = 400; // Bad request
+            let error_obj = {
+                status_code: status, 
+                backend_msg: "no", 
+                error_info: error.message, 
+                error_detail: Error(error).stack
+            };
+            throw error_obj;
+        }
+    }
+};
+
 // add try catch block to handle timeout.
 const send_logs = (type, obj) => async (dispatch)=>{
     if(process.env.REACT_APP_ENV === 'prod'){
@@ -917,4 +1174,12 @@ const send_logs = (type, obj) => async (dispatch)=>{
     }
 };
 
-export {setSearchColumn,setSearchSettings,fetchSearchIDs, fetchSearchResults, fetchCourse, getCourseEnrollInfo, getNTURatingData, getPTTData, getCourseSyllabusData, setFilter, setFilterEnable, fetchCourseTableCoursesByIds, fetchFavoriteCourses, createCourseTable, linkCoursetableToUser, fetchCourseTable, patchCourseTable, fetchUserById, registerNewUser, logOut, logIn, updateCourseTable, addFavoriteCourse, deleteUserProfile, deleteUserAccount, patchUserInfo, verify_recaptcha, request_otp_code, use_otp_link_student_id, setNewDisplayTags, send_logs };
+export 
+{ 
+    setSearchColumn, setSearchSettings, fetchSearchIDs, fetchSearchResults, fetchCourse, 
+    getCourseEnrollInfo, getNTURatingData, getPTTData, getCourseSyllabusData, 
+    fetchCourseTableCoursesByIds, fetchFavoriteCourses, createCourseTable, linkCoursetableToUser, fetchCourseTable, patchCourseTable, 
+    fetchUserById, registerNewUser, logOut, logIn, updateCourseTable, addFavoriteCourse, deleteUserProfile, deleteUserAccount, patchUserInfo, 
+    getSocialPostByPostId, getSocialPostByCourseId, createSocialPost, reportSocialPost, voteSocialPost, deleteSocialPost,
+    verify_recaptcha, request_otp_code, use_otp_link_student_id, setNewDisplayTags, send_logs, setFilter, setFilterEnable, 
+};
