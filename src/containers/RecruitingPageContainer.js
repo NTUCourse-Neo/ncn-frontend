e3b07385bdfd20938312f9fc0d2d3caf39f9c627
swh00tw <a6140000@gmail.com>
2022-03-02 21:21:51 +0800

    adjust RWD

--- src/containers/RecruitingPageContainer.js
+++ src/containers/RecruitingPageContainer.js
@@ -25,18 +25,17 @@ function RecruitingPageContainer(props){
     setPageMeta({title: `招募 | NTUCourse Neo`, desc: `招募頁面 | NTUCourse Neo，全新的臺大選課網站。`});
   } , [])
   return(
-    <>
-      <Flex h="95vh" direction="column" alignItems="center" px={["10", "20", "100","200"]} pt="100px">
+      <Flex direction="column" alignItems="center" px={["10", "20", "100","200"]} pt="100px">
         <HStack w="100%" align="end" justify="space-between">
-          <Image src={hiringOfficeSvg} alt="hire_office" w="20vw"/>
-          <Image src={hiringPeopleSvg} alt="hire_people" w="20vw"/>
-          <Image src={hiringCollabSvg} alt="hire_collab" w="20vw"/>
+          <Image src={hiringOfficeSvg} alt="hire_office" w={{ md: '25%'}} display={{base: 'none', md: 'inline-block'}}/>
+          <Image src={hiringPeopleSvg} alt="hire_people" w={{ base: '90%', md: '25%'}} />
+          <Image src={hiringCollabSvg} alt="hire_collab" w={{ md: '25%'}} display={{base: 'none', md: 'inline-block'}}/>
         </HStack>
-        <Flex h="600px" w="100%" flexDirection="row" justifyContent="space-between" alignItems="center" mt="20px">
-          <Flex w="100%" direction="column" alignItems="start" justifyContent="center">
-            <Text as="h1" fontSize="8xl" fontWeight="bold" color="gray.700">We are hiring!</Text>
+        <Flex my={4} w="100%" flexDirection={{base: 'column', lg: 'row'}} justifyContent="space-between" alignItems="center" mt="20px">
+          <Flex w="100%" direction="column" alignItems={{base: 'center', lg: 'start'}} justifyContent="center">
+            <Text as="h1" fontSize={{base: '5xl', md: '8xl'}} fontWeight="bold" color="gray.700">We are hiring!</Text>
             <Text mt="-20px" ml="2" as="h1" fontSize="5xl" fontWeight="500" color="gray.500">新夥伴招募</Text>
-            <Flex mt="8" w="600px" px="8" py="4" direction="column" alignItems="start" justifyContent="center" border="2px" borderColor="gray.400" borderRadius="lg">
+            <Flex mt="8" w={{base: '100%', md: '80%'}} px="8" py="4" direction="column" alignItems="start" justifyContent="center" border="2px" borderColor="gray.400" borderRadius="lg">
               <Flex w="100%" justifyContent="space-between" alignItems="center">
                 <HStack>
                   <Icon as={FaHandshake} boxSize="5" color="gray.500" />
@@ -62,7 +61,7 @@ function RecruitingPageContainer(props){
               </Flex>
             </Flex>
           </Flex>
-          <Flex w="70%" direction="column" alignItems="end" justifyContent="center">
+          <Flex my={4} w="70%" direction="column" alignItems="end" justifyContent="center">
             <InputGroup my="2" size='lg'>
               <Input variant='outline' placeholder={"姓名 / Name"} />
               <InputRightAddon children='必填' />
@@ -87,7 +86,6 @@ function RecruitingPageContainer(props){
           </Flex>
         </Flex>
       </Flex>
-    </>
   );
 }
 
