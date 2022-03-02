859a90a19f70fcd2697aa210d479af631b548ce8
jc-hiroto <taste.ful.msa@gmail.com>
2022-03-03 02:18:07 +0800

    Added policy popover.

--- src/containers/RecruitingPageContainer.js
+++ src/containers/RecruitingPageContainer.js
@@ -10,7 +10,14 @@ import {
     InputRightAddon,
     Button,
     Icon,
-    useToast
+    useToast,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverHeader,
+    PopoverBody
   } from '@chakra-ui/react';
 import ReCAPTCHA from "react-google-recaptcha";
 import { FaHandshake } from 'react-icons/fa';
@@ -144,18 +151,17 @@ function RecruitingPageContainer(props){
               </HStack>
               <Flex flexDirection="column" mt="2" w="100%" alignItems="start">
                 <Text fontSize="md" fontWeight="700" color="gray.500">In this role, you will...</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Co-develop the course selection service.</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Work with an innovative team.</Text>
+                <Text fontSize="md" fontWeight="500" color="gray.500">- Co-develop the course selection service to enhance user experience.</Text>
+                <Text fontSize="md" fontWeight="500" color="gray.500">- Work with innovative teammates.</Text>
                 <Text fontSize="md" fontWeight="500" color="gray.500">- Designing and developing APIs.</Text>
                 <Text fontSize="md" fontWeight="500" color="gray.500">- Staying abreast of developments in web applications and programming languages.</Text>
               </Flex>
               <Flex flexDirection="column" mt="2" w="100%" alignItems="start">
                 <Text fontSize="md" fontWeight="700" color="gray.500">Ideal candidate</Text>
                 <Text fontSize="md" fontWeight="500" color="gray.500">- Capability to teamwork and communicate.</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Familiar with React.js development.</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Familiar with Node.js development.</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Experience on Git. (perferred)</Text>
-                <Text fontSize="md" fontWeight="500" color="gray.500">- Experiences of DevOps or CI/CD. (perferred)</Text>
+                <Text fontSize="md" fontWeight="500" color="gray.500">- Familiar with React and Node.js development.</Text>
+                <Text fontSize="md" fontWeight="500" color="gray.500">- Experience on Git.</Text>
+                <Text fontSize="md" fontWeight="500" color="gray.500">- Experience on Docker and CI/CD tools is a plus.</Text>
               </Flex>
             </Flex>
           </Flex>
@@ -182,7 +188,41 @@ function RecruitingPageContainer(props){
               <Flex w="100%" flexDirection={{base: "column", md:"row", lg: "column"}} alignItems={{lg: "end"}} justifyContent={{base: "space-between"}} mt={{base: 4, md:0}}>
                 <HStack>
                   <Checkbox alignContent="center" onChange={()=>{setIsChecked(!isChecked)}}>我已閱讀並同意</Checkbox>
-                  <Text as="button" color="blue.500" size="sm"> <Text as="u">資料利用政策</Text></Text>
+                  <Popover placement='top-start'>
+                    <PopoverTrigger>
+                      <Text as="button" color="blue.500" size="sm"> <Text as="u">資料利用政策</Text></Text>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <PopoverArrow />
+                      <PopoverCloseButton />
+                      <PopoverHeader>
+                        <Text fontWeight="800">個人資料利用政策</Text>
+                      </PopoverHeader>
+                      <PopoverBody h={{base: "300px", md: "500px"}} overflow="auto">
+                      因應個人資料保護法及本團隊之個人資料管理規定，本團隊在向您蒐集個人資料之前，依法向您告知下列事項，當您勾選「我同意」，表示您已經閱讀、瞭解且同意接受下列之所有內容：<br/>
+                      <br/>
+                      <Text fontWeight="800">1. 蒐集目的及類別</Text>
+                      為提供團隊成員招聘需求及本團隊內部管理使用之蒐集目的，而須獲取您下列個人資料： 姓名、電話、E-mail 或其他得以直接或間接方式識別您個人之資料。 <br/>
+                      <br/>
+                      <Text fontWeight="800">2.個人資料利用之期間、地區、對象及方式</Text>
+                      您的個人資料，除涉及國際業務或活動外，僅供本團隊及相關業務合作夥伴，於上述蒐集目的之必要合理範圍內加以利用至前述蒐集目的消失時為止。 <br/>
+                      <br/>
+                      <Text fontWeight="800">3. 當事人權利行使</Text>
+                      依據個人資料保護法第3條規定，您可向本團隊請求查詢或閱覽、製給複製、補充或更正、停止蒐集/處理/利用或刪除您的個人資料。 <br/>
+                      <br/>
+                      <Text fontWeight="800">4. 不提供個人資料之權益影響</Text>
+                      如您不提供或未提供正確之個人資料，或要求停止蒐集/處理/利用/刪除個人資料、服務訊息的取消訂閱，本團隊將無法為您提供蒐集目的之相關服務。 <br/>
+                      <br/>
+                      <Text fontWeight="800">5. 第三方服務供應商</Text>
+                      本辦法僅列出本平台服務所需之資料運用相關條款，關於使用牽涉本平台之第三方服務之資料收集與管理辦法，請自行參閱各服務供應商之文件。除本規章外，若行為違反第三方服務之任何守則導致之相關後果，不在本伺服器管理範圍內，與本平台無涉。 <br/>
+                      <br/>
+                      <Text fontWeight="800">個人資料同意提供： </Text>
+                      一、本人確已閱讀並瞭解上述告知事項，並勾選「我同意」授權貴團隊於所列目的之必要合理範圍內，蒐集、處理及利用本人之個人資料。 <br/>
+                      <br/>
+                      二、本人瞭解此同意書符合個人資料保護法及相關法規之要求，並提供予貴團隊留存及日後查證使用。 <br/>
+                      </PopoverBody>
+                    </PopoverContent>
+                  </Popover>
                 </HStack>
                 <Button isDisabled={!isChecked || !isFilledRequiredFields() || !isCaptchaVerified}  isLoading={isSubmiting} size='md' colorScheme='blue' mt={{base: 5, md: 3}} w={{base: '100%', md: '150px'}} onClick={()=>{onSubmit()}}>送出 / Submit</Button>
               </Flex>
