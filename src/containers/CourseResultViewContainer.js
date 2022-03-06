14959aa252af5c6357e87a98d853f78b5a9b5291
jc-hiroto <taste.ful.msa@gmail.com>
2022-03-07 00:20:05 +0800

    Fixed coursetable toggle button.

--- src/containers/CourseResultViewContainer.js
+++ src/containers/CourseResultViewContainer.js
@@ -46,9 +46,10 @@ import {
     AlertDialogFooter,
     Alert,
     AlertIcon,
+    Icon,
 } from '@chakra-ui/react';
 import { BeatLoader } from 'react-spinners';
-import { FaAngleLeft, FaChevronDown, FaChevronUp, FaPlus, FaMinus, FaArrowRight, FaRss } from 'react-icons/fa';
+import { FaChevronDown, FaChevronUp, FaPlus, FaMinus, FaArrowRight, FaRss, FaRegCalendarAlt } from 'react-icons/fa';
 import CourseInfoRowContainer from './CourseInfoRowContainer';
 import FilterModal from '../components/FilterModal';
 import CourseSearchInput from '../components/CourseSearchInput';
@@ -546,10 +547,10 @@ function CourseResultViewContainer() {
                 </Box>
             </Flex>
             <Fade in={!displayTable}>
-                <Flex as="button" flexDirection={isMobile? "row":"column"} alignItems="center" justifyContent="center" position="absolute" top={isMobile? "85vh":"80vh"} left={isMobile? "70vw":"90vw"} bg="gray.100" boxShadow="md" py={isMobile?"1":"4"} px="2" borderRadius="xl"
+                <Flex as="button" flexDirection="row" alignItems="center" justifyContent="center" position="absolute" top={isMobile? "85vh":"80vh"} left={isMobile? "70vw":"90vw"} bg="gray.100" boxShadow="md" py="1" px="4" borderRadius="xl"
                 onClick={() => setDisplayTable(!displayTable)} _hover={{boxShadow:"lg", transform:"translateY(-2px) scale(1.02)"}} transition="all 200ms">
-                  <FaAngleLeft size={24}/>
-                    <Text my="2" fontWeight={800} fontSize={isMobile? "md":"lg"} color="gray.600" style={isMobile?{}:{writingMode: "vertical-rl"}}>課表</Text>
+                  <Icon mr="1" as={FaRegCalendarAlt} boxSize="4" color="teal.500" />
+                  <Text my="2" fontWeight={800} fontSize={isMobile? "md":"lg"} color="gray.600">課表</Text>
                 </Flex>
             </Fade>
             <Collapse in={displayTable} animateOpacity>
