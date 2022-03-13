551526e2befb4490379e25c7124d3e567d071700
swh00tw <a6140000@gmail.com>
2022-03-13 23:12:22 +0800

    Change props to destructive style: HomeCard

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -6,23 +6,23 @@ import {
     Text,
     Image,
   } from '@chakra-ui/react';
-function HomeCard(props) {
+function HomeCard({bg, desc, img, title, children}) {
     return (
-        <Box className="initialBox" w="80vw" bg={props.bg} borderRadius="3xl" p={4} boxShadow='xl'>
+        <Box className="initialBox" w="80vw" bg={bg} borderRadius="3xl" p={4} boxShadow='xl'>
             <Flex justifyContent="start" alignItems="start" flexDirection="column" m="8">
-              <Heading as="h1" fontSize={["xl", "2xl","3xl", "5xl"]} fontWeight="800" color="gray.700" mb="8">{props.title}</Heading>
+              <Heading as="h1" fontSize={["xl", "2xl","3xl", "5xl"]} fontWeight="800" color="gray.700" mb="8">{title}</Heading>
               <Flex w="100%" justifyContent="start" alignItems="center" flexDirection="row" flexWrap="wrap-reverse" css={{ gap: '2em' }}>
                 <Flex w={["100%", "100%","100%", "60%"]} justifyContent="start" alignItems="start" flexDirection="column" mr="4">
-                  {props.desc.map((item,index) => {
+                  {desc.map((item,index) => {
                     return (
                       <Text key={index} fontSize={["md", "lg","lg", "3xl"]} fontWeight="400" color="gray.600" mb={4}>{item}</Text>
                     );
                   })}
                 </Flex>
-                <Image src={props.img} alt={props.title} w={["100%", "100%","100%", "30%"]} borderRadius="xl" boxShadow="xl" _hover={{ transform: "Scale(1.2)", boxShadow: "2xl"}} transition="all ease-out 0.2s"/>
+                <Image src={img} alt={title} w={["100%", "100%","100%", "30%"]} borderRadius="xl" boxShadow="xl" _hover={{ transform: "Scale(1.2)", boxShadow: "2xl"}} transition="all ease-out 0.2s"/>
               </Flex>
             </Flex>
-            {props.children}
+            {children}
           </Box>
     );
 }
