9da5d688ad9bae01d791f5348fe9d6f1f1afebde
swh00tw <a6140000@gmail.com>
2022-05-01 22:33:50 +0800

    add filter modal skeleton code

---
+++ src/components/FilterModals/DeptFilterModal.js
@@ -0,0 +1,129 @@
+import {
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Badge,
+    Button,
+    Heading,
+    Divider,
+    Flex,
+    useMediaQuery,
+} from '@chakra-ui/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function DeptFilterModal({ title, isEnabled, selectedDept, setSelectedDept }) {
+    const dispatch = useDispatch();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [isMobile] = useMediaQuery('(max-width: 760px)');
+
+    const onOpenModal = () => {
+        // TODO: overwrite local states by redux store
+        onOpen();
+    };
+
+    const onCancelEditing = () => {
+        // fire when click "X" or outside of modal
+        // TODO: overwrite local state by redux state
+        onClose();
+    };
+
+    const onSaveEditing = () => {
+        // fire when click "Save"
+        // TODO: overwrite redux state by local state
+        onClose();
+    };
+
+    const onResetEditing = () => {
+        // fire when click "Reset"
+        // TODO: set local state to empty array
+    };
+
+    return (
+        <>
+            <Button
+                size={isMobile ? 'sm' : 'md'}
+                isDisabled={!isEnabled}
+                onClick={() => {
+                    onOpenModal();
+                }}
+            >
+                {title}
+            </Button>
+            <Modal
+                isOpen={isOpen}
+                onClose={() => {
+                    onCancelEditing();
+                }}
+                size={isMobile ? 'full' : 'xl'}
+                scrollBehavior="inside"
+            >
+                <ModalOverlay />
+                <ModalContent maxW={isMobile ? '' : '50vw'}>
+                    <ModalHeader>
+                        {title}
+                        {isMobile ? (
+                            <Flex flexDirection="row" justifyContent="start" alignItems="center" mt="2">
+                                <Button
+                                    size="sm"
+                                    colorScheme="blue"
+                                    mr={3}
+                                    onClick={() => {
+                                        onSaveEditing();
+                                    }}
+                                >
+                                    套用
+                                </Button>
+                                <Button
+                                    size="sm"
+                                    variant="ghost"
+                                    onClick={() => {
+                                        onResetEditing();
+                                    }}
+                                >
+                                    重設
+                                </Button>
+                            </Flex>
+                        ) : (
+                            <></>
+                        )}
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody overflow="auto" pt="0">
+                        {/* TODO: render buttons */}
+                        <h1>hello</h1>
+                    </ModalBody>
+                    {isMobile ? (
+                        <></>
+                    ) : (
+                        <ModalFooter>
+                            <Button
+                                colorScheme="blue"
+                                mr={3}
+                                onClick={() => {
+                                    onSaveEditing();
+                                }}
+                            >
+                                套用
+                            </Button>
+                            <Button
+                                variant="ghost"
+                                onClick={() => {
+                                    onResetEditing();
+                                }}
+                            >
+                                重設
+                            </Button>
+                        </ModalFooter>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
+
+export default DeptFilterModal;
---
+++ src/components/FilterModals/components/FilterElement.js
@@ -0,0 +1,5 @@
+// Buttons in filter modal
+function FilterButton({ id, name }) {
+    return <h1>123</h1>;
+}
+export default FilterButton;
