35bc1992e281941702ffb5c9edf31c3c3ef50053
swh00tw <a6140000@gmail.com>
2022-05-02 00:04:19 +0800

    finish category modal

--- src/components/FilterModals/CategoryFilterModal.js
+++ src/components/FilterModals/CategoryFilterModal.js
@@ -7,7 +7,6 @@ import {
     ModalFooter,
     ModalBody,
     ModalCloseButton,
-    Badge,
     Button,
     Heading,
     Divider,
@@ -15,33 +14,87 @@ import {
     useMediaQuery,
 } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { type_list, code_map } from '../../data/course_type';
+import FilterElement from './components/FilterElement';
+import { setFilter } from '../../actions';
+import React, { useMemo } from 'react';
 
 function CategoryFilterModal({ title, isEnabled, selectedType, setSelectedType }) {
     const dispatch = useDispatch();
+    const search_filters = useSelector(state => state.search_filters);
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [isMobile] = useMediaQuery('(max-width: 760px)');
 
     const onOpenModal = () => {
-        // TODO: overwrite local states by redux store
+        // overwrite local states by redux store
+        setSelectedType(search_filters.category);
         onOpen();
     };
 
     const onCancelEditing = () => {
         // fire when click "X" or outside of modal
-        // TODO: overwrite local state by redux state
+        // overwrite local state by redux state
         onClose();
+        setSelectedType(search_filters.category);
     };
 
     const onSaveEditing = () => {
         // fire when click "Save"
-        // TODO: overwrite redux state by local state
+        // overwrite redux state by local state
+        dispatch(setFilter('category', selectedType));
         onClose();
     };
 
     const onResetEditing = () => {
         // fire when click "Reset"
-        // TODO: set local state to empty array
+        // set local state to empty array
+        setSelectedType([]);
     };
+
+    const modalBody = useMemo(
+        () => (
+            <React.Fragment key={`modalBody`}>
+                {Object.keys(code_map).map((code_map_key, index) => {
+                    const categories = type_list.filter(type => type.code[0] === code_map_key);
+                    return (
+                        <React.Fragment key={`${code_map_key}`}>
+                            <Flex
+                                key={`${code_map_key}-${index}`}
+                                px="2"
+                                h="40px"
+                                flexDirection="column"
+                                justifyContent="center"
+                                position="sticky"
+                                top="0"
+                                mt={index === 0 ? 0 : 6}
+                                bgColor="white"
+                                zIndex="50"
+                            >
+                                <Heading fontSize="2xl" color="gray.600">
+                                    {code_map[code_map_key].name}
+                                </Heading>
+                                <Divider />
+                            </Flex>
+                            {categories
+                                .filter(type => !selectedType.includes(type.id))
+                                .map((type, typeIdx) => (
+                                    <FilterElement
+                                        key={`${type.id}-${typeIdx}`}
+                                        id={type.code}
+                                        name={type.full_name}
+                                        selected={false}
+                                        onClick={() => {
+                                            setSelectedType([...selectedType, type.id]);
+                                        }}
+                                    />
+                                ))}
+                        </React.Fragment>
+                    );
+                })}
+            </React.Fragment>
+        ),
+        [selectedType, setSelectedType]
+    );
 
     return (
         <>
@@ -94,8 +147,21 @@ function CategoryFilterModal({ title, isEnabled, selectedType, setSelectedType }
                     </ModalHeader>
                     <ModalCloseButton />
                     <ModalBody overflow="auto" pt="0">
-                        {/* TODO: render buttons */}
-                        <h1>hello</h1>
+                        {type_list
+                            .filter(type => selectedType.includes(type.id))
+                            .map((type, index) => (
+                                <FilterElement
+                                    key={`${type.id}-${index}-modalHeader`}
+                                    id={type.code}
+                                    name={type.full_name}
+                                    selected={true}
+                                    onClick={() => {
+                                        setSelectedType(selectedType.filter(id => id !== type.id));
+                                    }}
+                                />
+                            ))}
+                        <Divider />
+                        {modalBody}
                     </ModalBody>
                     {isMobile ? (
                         <></>
