f60216162af7daabccadeb058b6e5806f37a6a34
swh00tw <a6140000@gmail.com>
2022-05-29 00:00:59 +0800

    eliminate uique key warning

--- src/containers/CourseTableContainer.js
+++ src/containers/CourseTableContainer.js
@@ -1,5 +1,6 @@
 import { Table, Thead, Tbody, Tr, Th, Td, Flex, Center, Box, Text, Skeleton, Tooltip } from "@chakra-ui/react";
-import { useState } from "react";
+import * as React from "react";
+import { useState, useCallback } from "react";
 import CourseTableCard from "components/CourseTableCard/CourseTableCard";
 import { weekdays_map } from "data/mapping_table";
 
@@ -7,40 +8,45 @@ function CourseTableContainer({ courses, loading, courseTimes, hoveredCourse, ho
   const days = ["1", "2", "3", "4", "5"];
   const interval = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D"];
   const [activeDayCol, setActiveDayCol] = useState(0);
-  const renderCourseBox = (course, hover, day, interval, grow) => {
-    if (course.time_map[day][interval].includes(hover.time_map[day][interval][0])) {
+
+  const renderCourseTableCard = useCallback(
+    (course, hover, day, interval, grow) => {
+      if (course.time_map[day][interval].includes(hover.time_map[day][interval][0])) {
+        return (
+          <CourseTableCard
+            grow={grow}
+            hoverId={hover.time_map[day][interval][0]}
+            courseTime={course.time_map[day][interval]}
+            courseData={courses}
+            interval={interval}
+            day={weekdays_map[day]}
+          />
+        );
+      }
       return (
-        <CourseTableCard
-          grow={grow}
-          hoverId={hover.time_map[day][interval][0]}
-          courseTime={course.time_map[day][interval]}
-          courseData={courses}
-          interval={interval}
-          day={weekdays_map[day]}
-        />
+        <>
+          <CourseTableCard
+            grow={grow}
+            isHover
+            courseTime={course.time_map[day][interval]}
+            courseData={hover.course_data}
+            interval={interval}
+            day={weekdays_map[day]}
+          />
+          <CourseTableCard grow={grow} courseTime={course.time_map[day][interval]} courseData={courses} interval={interval} day={weekdays_map[day]} />
+        </>
       );
-    }
-    return (
-      <>
-        <CourseTableCard
-          grow={grow}
-          isHover
-          courseTime={course.time_map[day][interval]}
-          courseData={hover.course_data}
-          interval={interval}
-          day={weekdays_map[day]}
-        />
-        <CourseTableCard grow={grow} courseTime={course.time_map[day][interval]} courseData={courses} interval={interval} day={weekdays_map[day]} />
-      </>
-    );
-  };
+    },
+    [courses]
+  );
+
   const renderTableContent = (days, interval, i, grow) => {
     return (
-      <Tr key={i + "_Tr"}>
+      <>
         {days.map((day, j) => {
           if (loading) {
             return (
-              <Td>
+              <Td key={`${day}-${i}-${j}`}>
                 <Skeleton borderRadius="lg" speed={1 + (i + j) * 0.1}>
                   <Box h="12" w="4vw" />
                 </Skeleton>
@@ -49,10 +55,10 @@ function CourseTableContainer({ courses, loading, courseTimes, hoveredCourse, ho
           }
           if (courseTimes.time_map && day in courseTimes.time_map && interval in courseTimes.time_map[day]) {
             if (hoveredCourse && hoveredCourseTime && day in hoveredCourseTime.time_map && interval in hoveredCourseTime.time_map[day]) {
-              return <Td>{renderCourseBox(courseTimes, hoveredCourseTime, day, interval, grow)}</Td>;
+              return <Td key={`${day}-${i}-${j}`}>{renderCourseTableCard(courseTimes, hoveredCourseTime, day, interval, grow)}</Td>;
             }
             return (
-              <Td key={i + "_" + j}>
+              <Td key={`${day}-${i}-${j}`}>
                 <CourseTableCard
                   grow={grow}
                   courseTime={courseTimes.time_map[day][interval]}
@@ -65,7 +71,7 @@ function CourseTableContainer({ courses, loading, courseTimes, hoveredCourse, ho
           }
           if (hoveredCourse && hoveredCourseTime && day in hoveredCourseTime.time_map && interval in hoveredCourseTime.time_map[day]) {
             return (
-              <Td key={i + "_" + j}>
+              <Td key={`${day}-${i}-${j}`}>
                 <CourseTableCard
                   grow={grow}
                   isHover
@@ -78,7 +84,7 @@ function CourseTableContainer({ courses, loading, courseTimes, hoveredCourse, ho
             );
           }
           return (
-            <Td key={i + "_" + j}>
+            <Td key={`${day}-${i}-${j}`}>
               <Flex w="100%" h="3vh" mb="1" justifyContent="center" alignItems="center">
                 <Text color="gray.300" fontSize="5xl" fontWeight="700" key={i + "_" + j}>
                   {interval}
@@ -87,49 +93,46 @@ function CourseTableContainer({ courses, loading, courseTimes, hoveredCourse, ho
             </Td>
           );
         })}
-      </Tr>
+      </>
     );
   };
-  const renderTable = () => {
-    return (
-      <Table variant="simple" colorScheme="blue" borderRadius="lg" w="100%">
-        <Thead>
-          {activeDayCol === 0 ? (
-            <Tr>
-              {days.map((day, j) => {
-                return (
-                  <Tooltip hasArrow placement="top" label="點擊展開單日" bg="gray.600" color="white">
-                    <Th key={day + "_Th"} onClick={() => setActiveDayCol(day)} cursor="pointer">
-                      <Center key={day + "_Center"}>{weekdays_map[day]}</Center>
-                    </Th>
-                  </Tooltip>
-                );
-              })}
-            </Tr>
-          ) : (
-            <Tr>
-              {
-                <Tooltip hasArrow placement="top" label="點擊收合" bg="gray.600" color="white">
-                  <Th onClick={() => setActiveDayCol(0)} cursor="pointer">
-                    <Center>{weekdays_map[activeDayCol]}</Center>
+  return (
+    <Table variant="simple" colorScheme="blue" borderRadius="lg" w="100%">
+      <Thead>
+        {activeDayCol === 0 ? (
+          <Tr>
+            {days.map((day, j) => {
+              return (
+                <Tooltip hasArrow placement="top" label="點擊展開單日" bg="gray.600" color="white" key={`${day}-${j}`}>
+                  <Th key={day + "_Th"} onClick={() => setActiveDayCol(day)} cursor="pointer">
+                    <Center key={day + "_Center"}>{weekdays_map[day]}</Center>
                   </Th>
                 </Tooltip>
-              }
-            </Tr>
-          )}
-        </Thead>
-        <Tbody>
-          {interval.map((interval, i) => {
-            if (activeDayCol === 0) {
-              return renderTableContent(days, interval, i, false);
+              );
+            })}
+          </Tr>
+        ) : (
+          <Tr>
+            {
+              <Tooltip hasArrow placement="top" label="點擊收合" bg="gray.600" color="white">
+                <Th onClick={() => setActiveDayCol(0)} cursor="pointer">
+                  <Center>{weekdays_map[activeDayCol]}</Center>
+                </Th>
+              </Tooltip>
             }
-            return renderTableContent([activeDayCol], interval, i, true);
-          })}
-        </Tbody>
-      </Table>
-    );
-  };
-  return <>{renderTable()}</>;
+          </Tr>
+        )}
+      </Thead>
+      <Tbody>
+        {interval.map((interval, i) => {
+          if (activeDayCol === 0) {
+            return <Tr key={`${interval}-${i}`}>{renderTableContent(days, interval, i, false)}</Tr>;
+          }
+          return <Tr key={`${interval}-${i}`}>{renderTableContent([activeDayCol], interval, i, true)}</Tr>;
+        })}
+      </Tbody>
+    </Table>
+  );
 }
 
 export default CourseTableContainer;
--- src/containers/SideCourseTableContainer.js
+++ src/containers/SideCourseTableContainer.js
@@ -60,7 +60,6 @@ function SideCourseTableContainer({ isDisplay, setIsDisplay, setCourseIds, hover
 
   const [loading, setLoading] = useState(true);
   const [expired, setExpired] = useState(false);
-  console.error = () => {};
 
   const parseCourseDateTime = (course, course_time_tmp) => {
     // eslint-disable-next-line array-callback-return
