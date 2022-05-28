6fd9988359701e8ed8bf907843dcd585a989d709
swh00tw <a6140000@gmail.com>
2022-05-29 02:06:55 +0800

    delete bad code

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,8 +1,6 @@
 function setPageMeta({ title, desc }) {
-  title = title || "NTUCourse Neo";
-  desc = desc || "全新設計的台大選課網站。";
-  document.title = title;
-  document.querySelector("meta[name='description']").setAttribute("content", desc);
+  document.title = title || "NTUCourse Neo";
+  document.querySelector("meta[name='description']").setAttribute("content", desc || "全新設計的台大選課網站。");
 }
 
 export default setPageMeta;
