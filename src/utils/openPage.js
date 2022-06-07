b4d593b526c3f48053fc40db91a9367e0fbe1655
swh00tw <a6140000@gmail.com>
2022-06-07 23:38:46 +0800

    clear log

--- src/utils/openPage.js
+++ src/utils/openPage.js
@@ -2,7 +2,6 @@ function openPage(url, closeAfterOneSecond = false) {
   if (window) {
     const wnd = window.open(url, "_blank");
     if (closeAfterOneSecond) {
-      // console.log("closing");
       setTimeout(() => {
         wnd.close();
       }, 1000);
