1f3b7d38f23bb3bff630bcb447c590e385e62a82
swh00tw <a6140000@gmail.com>
2022-06-08 12:19:47 +0800

    refactor infoRowContainer

--- src/containers/CourseInfoRowContainer.js
+++ src/containers/CourseInfoRowContainer.js
@@ -3,7 +3,7 @@ import CourseInfoRow from "components/CourseInfoRow";
 import { Box, Flex, Spacer, Accordion } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
-function CourseInfoRowContainer({ courseInfo, w, setHoveredCourse, selectedCourses, displayTable, displayTags }) {
+function CourseInfoRowContainer({ courseInfo, setHoveredCourse, selectedCourses, displayTable, displayTags }) {
   const userInfo = useSelector((state) => state.user);
 
   // const hide_scroll_bar = {
@@ -11,37 +11,33 @@ function CourseInfoRowContainer({ courseInfo, w, setHoveredCourse, selectedCours
   //         display: "none"
   //     },
   // }
-
-  const renderCourseInfoRow = () => {
-    return courseInfo.map((info, index) => {
-      return (
-        <Accordion
-          allowToggle
-          w={{ base: "90vw", md: w }}
-          key={index}
-          onMouseEnter={() => setHoveredCourse(info)}
-          onMouseLeave={() => {
-            setHoveredCourse(null);
-          }}
-        >
-          <CourseInfoRow
-            id={info["id"]}
-            index={index}
-            courseInfo={info}
-            selected={selectedCourses.includes(info._id)}
-            setHoveredCourse={setHoveredCourse}
-            displayTags={displayTags}
-            displayTable={displayTable}
-            isfavorite={userInfo === null ? false : userInfo.db.favorites.includes(info._id)}
-          />
-          <Spacer my={{ base: 2, md: 1 }} />
-        </Accordion>
-      );
-    });
-  };
   return (
     <Box>
-      <Flex direction="column">{renderCourseInfoRow()}</Flex>
+      <Flex direction="column">
+        {courseInfo.map((info, index) => (
+          <Accordion
+            allowToggle
+            w={{ base: "90vw", md: "100%" }}
+            key={index}
+            onMouseEnter={() => setHoveredCourse(info)}
+            onMouseLeave={() => {
+              setHoveredCourse(null);
+            }}
+          >
+            <CourseInfoRow
+              id={info["id"]}
+              index={index}
+              courseInfo={info}
+              selected={selectedCourses.includes(info._id)}
+              setHoveredCourse={setHoveredCourse}
+              displayTags={displayTags}
+              displayTable={displayTable}
+              isfavorite={userInfo === null ? false : userInfo.db.favorites.includes(info._id)}
+            />
+            <Spacer my={{ base: 2, md: 1 }} />
+          </Accordion>
+        ))}
+      </Flex>
     </Box>
   );
 }
