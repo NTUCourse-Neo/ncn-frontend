fed2fd10097a67def58d70c37863ffd893cb716c
swh00tw <a6140000@gmail.com>
2022-06-19 12:19:56 +0800

    refactor skeleton row

--- src/components/SkeletonRow.js
+++ src/components/SkeletonRow.js
@@ -1,40 +1,35 @@
 import React from "react";
-import { Flex, useToast, Text, VisuallyHidden } from "@chakra-ui/react";
+import { Flex, useToast, Text } from "@chakra-ui/react";
 import { HashLoader } from "react-spinners";
 import { FaSadTear } from "react-icons/fa";
 
 function SkeletonRow({ loading, error }) {
   const toast = useToast();
-  const visible_logic = () => {
-    if (loading) {
-      return (
-        <Flex p="4">
-          <HashLoader size="60px" color="teal" />
-        </Flex>
-      );
-    } else if (error) {
-      return (
-        <>
-          <Text color="gray.300" pt="10">
-            {" "}
-            <FaSadTear size={32} />{" "}
-          </Text>
-          <VisuallyHidden>
-            {toast({
-              title: "錯誤",
-              description: "😢 哭阿，發生錯誤了，請稍後再試一次。",
-              status: "error",
-              duration: 5000,
-              isClosable: true,
-            })}
-          </VisuallyHidden>
-        </>
-      );
-    } else {
-      return <></>;
-    }
-  };
 
-  return visible_logic();
+  if (loading) {
+    return (
+      <Flex p="4">
+        <HashLoader size="60px" color="teal" />
+      </Flex>
+    );
+  }
+
+  if (error) {
+    toast({
+      title: "錯誤",
+      description: "😢 哭阿，發生錯誤了，請稍後再試一次。",
+      status: "error",
+      duration: 5000,
+      isClosable: true,
+    });
+    return (
+      <Text color="gray.300" pt="10">
+        {" "}
+        <FaSadTear size={32} />{" "}
+      </Text>
+    );
+  }
+
+  return null;
 }
 export default SkeletonRow;
