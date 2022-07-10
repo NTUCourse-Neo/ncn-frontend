8f89453a01609ec54c6971dcfb5c2aedd6e2b929
swh00tw <a6140000@gmail.com>
2022-07-11 00:56:56 +0800

    try to solve app error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "my-nextJs-site",
-      script: "./node_modules/next/dist/bin/next",
+      name: "ncn-frontend-v2",
+      script:
+        "./node_modules/next/dist/bin/next build && ./node_modules/next/dist/bin/next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
