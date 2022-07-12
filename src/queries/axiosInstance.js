cb3b043c75bae1b14d1f579d06e3b32dd551fe4a
swh00tw <a6140000@gmail.com>
2022-07-13 01:03:26 +0800

    delete old CI yaml

--- src/queries/axiosInstance.js
+++ src/queries/axiosInstance.js
@@ -1,8 +1,4 @@
 import axios from "axios";
-console.log(
-  "ENDPOINT: ",
-  process.env.NEXT_PUBLIC_API_ENDPOINT + process.env.NEXT_PUBLIC_API_VERSION
-);
 
 const instance = axios.create({
   baseURL:
