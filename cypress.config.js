22b3e913d8ea8ec70af0748f679afa451de3610e
swh00tw <a6140000@gmail.com>
2022-07-20 18:27:10 +0800

    update cypress files

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
   projectId: "79x2jm",
   env: {
     API_ENDPOINT: "",
-    API_VERSION: "",
   },
   e2e: {
     // We've imported your old cypress plugins here.
--- cypress/e2e/e2e/course.js
+++ cypress/e2e/e2e/course.js
@@ -5,12 +5,9 @@ describe("Course related functionalties test", () => {
     cy.url().should("include", "/course");
   });
   it("Search without typing anything", () => {
-    cy.intercept(
-      "POST",
-      `${Cypress.env("API_ENDPOINT")}${Cypress.env(
-        "API_VERSION"
-      )}/courses/search`
-    ).as("search-courses");
+    cy.intercept("POST", `${Cypress.env("API_ENDPOINT")}/v2/courses/search`).as(
+      "search-courses"
+    );
     cy.get(".css-bs7b3k").contains("搜尋").click();
 
     cy.wait(["@search-courses"]);
