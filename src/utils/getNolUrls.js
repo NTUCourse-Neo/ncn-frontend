625d10e9c671b2cdf7ba628ba2ece13a88af7bde
jc-hiroto <taste.ful.msa@gmail.com>
2022-07-27 16:14:06 +0800

    Fixed minor type.

--- src/utils/getNolUrls.js
+++ src/utils/getNolUrls.js
@@ -6,7 +6,7 @@ const getNolUrl = (course) => {
   const lang = "CH";
   const base_url = "https://nol.ntu.edu.tw/nol/coursesearch/print_table.php?";
   const course_id = course.identifier.replace("E", "");
-  const dept_id = course.departments[0]?.id ?? "T010";
+  const dept_id = course.departments?.[0]?.id ?? "T010";
   const class_id = course?.class ?? "";
   const params = `course_id=${course_id.substr(0, 3)}%20${course_id.substr(
     3
