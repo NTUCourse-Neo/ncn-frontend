ba0da86f3c1d8785668bd15e7b5ed38426bee258
swh00tw <a6140000@gmail.com>
2022-07-29 15:44:51 +0800

    refactor sendLogs

--- src/queries/sendLogs.js
+++ src/queries/sendLogs.js
@@ -3,8 +3,8 @@ const api_version = "v1";
 
 const sendLogs = async (type, obj) => {
   if (process.env.NEXT_PUBLIC_ENV === "prod") {
-    const resp = await instance.post(`${api_version}/logs/${type}`, obj);
-    return resp.data;
+    const { data } = await instance.post(`${api_version}/logs/${type}`, obj);
+    return data;
   } else {
     console.log("[INFO] Logs are not sent to server in development mode.");
   }
