8ebe82d878a1531b3be8f8dae5868a280479ed5b
swh00tw <a6140000@gmail.com>
2022-08-02 12:51:32 +0800

    add send logs type

--- src/queries/sendLogs.ts
+++ src/queries/sendLogs.ts
@@ -1,7 +1,10 @@
 import instance from "queries/axiosInstance";
 const api_version = "v1";
 
-const sendLogs = async (type, obj) => {
+const sendLogs = async (
+  type: "info" | "error",
+  obj: Record<string, unknown>
+) => {
   if (process.env.NEXT_PUBLIC_ENV === "prod") {
     const { data } = await instance.post(`${api_version}/logs/${type}`, obj);
     return data;
