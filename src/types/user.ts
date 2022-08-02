80d31f2dca245733b22c84aaf26412f248244b02
swh00tw <a6140000@gmail.com>
2022-08-02 11:30:55 +0800

    add user & course table type

---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+import { Department, Course } from "@/types/course";
+
+export interface User {
+  db: {
+    id: string;
+    name: string;
+    email: string;
+    year: number;
+    course_tables: string[];
+    d_major: Department | null;
+    major: Department | null;
+    minors: Department[];
+    favorites: Course[];
+    history_courses: unknown; // TODO
+    student_id: null | string; // TODO
+    languages: unknown; // TODO: to be removed
+  };
+}
