6ff5cba1a6f038ad914d8ac0ab308df6758e1d98
swh00tw <a6140000@gmail.com>
2022-08-02 13:32:40 +0800

    convert customFetch to ts

--- src/utils/CustomFetch.ts
+++ src/utils/CustomFetch.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 // used for hitting self-hosted API in Next.js
-const handleFetch = async (route, payload) => {
+const handleFetch = async (route: string, payload: object) => {
   const response = await axios.post(route, payload);
-  return response.data;
+  return response.data as unknown;
 };
 
 export default handleFetch;
