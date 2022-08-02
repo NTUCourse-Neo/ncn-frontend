97305a5b20354a31110a9438a0c219091d67017a
swh00tw <a6140000@gmail.com>
2022-08-02 13:39:19 +0800

    convert ga.js to ts

--- src/utils/ga.ts
+++ src/utils/ga.ts
@@ -1,4 +1,8 @@
-export const reportEvent = (event_category, eventAction, event_label) => {
+export const reportEvent = (
+  event_category: string,
+  eventAction: string,
+  event_label: string
+) => {
   if (process.env.NEXT_PUBLIC_ENV === "prod") {
     window.gtag("event", eventAction, { event_category, event_label });
   }
