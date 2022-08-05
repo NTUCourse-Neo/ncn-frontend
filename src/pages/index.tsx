4b0d7994cac27feae711e89f5d1e4c9bec290c52
swh00tw <a6140000@gmail.com>
2022-08-05 16:05:24 +0800

    test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -147,7 +147,7 @@ function HomePage() {
   const [displayingCard, setDisplayingCard] = useState(0);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { user, isLoading: isAuthLoading } = useUser();
-  useUserInfo(user?.sub ?? null, {
+  useUserInfo(user?.sub, {
     onSuccessCallback: async (userData, key, config) => {
       if (!userData?.user?.db && user?.email) {
         setIsRegistering(true);
