ae75a13a6247662d0acf5a0cdbdbc9e24f3c28d4
swh00tw <a6140000@gmail.com>
2022-09-17 14:21:44 +0800

    add comment

--- src/hooks/useOutsideDetecter.ts
+++ src/hooks/useOutsideDetecter.ts
@@ -4,15 +4,14 @@ interface IdNode extends Node {
   id?: string;
 }
 
+// https://stackoverflow.com/a/42234988
 export default function useOutsideDetecter(
   ref: React.RefObject<HTMLDivElement>,
   id: string,
   callback: () => void
 ) {
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
+    // Alert if clicked on outside of element
     function handleClickOutside(event: MouseEvent) {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         if ((event?.target as IdNode)?.id !== id) {
