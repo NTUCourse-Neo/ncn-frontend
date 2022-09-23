25c68f08f318ca8da41ddd4059bf115e7fe0c909
swh00tw <a6140000@gmail.com>
2022-09-23 11:26:58 +0800

    delete unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,6 @@ import {
   Icon,
   Tooltip,
   useColorModeValue,
-  Image,
-  SpacerProps,
   InputGroup,
   InputRightElement,
   Input,
@@ -26,9 +24,7 @@ import {
   Menu,
   MenuButton,
   MenuList,
-  MenuItemOption,
   HStack,
-  MenuOptionGroup,
   FlexProps,
   Checkbox,
   Radio,
@@ -38,19 +34,10 @@ import {
   Stack,
   Badge,
 } from "@chakra-ui/react";
-import {
-  FaArrowDown,
-  FaArrowRight,
-  FaGithub,
-  FaChevronDown,
-} from "react-icons/fa";
-import Link from "next/link";
+import { FaArrowRight, FaChevronDown } from "react-icons/fa";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import { scroller, Element as ScrollAnchorElement } from "react-scroll";
 import { BeatLoader } from "react-spinners";
-import HomeCard from "components/HomeCard";
-import { DiscordIcon } from "components/CustomIcons";
 import useUserInfo from "hooks/useUserInfo";
 import { useUser } from "@auth0/nextjs-auth0";
 import handleFetch from "utils/CustomFetch";
@@ -73,17 +60,6 @@ function setCheckList<T>(array: T[], member: T): T[] {
   } else {
     return array.filter((item) => item !== member);
   }
-}
-
-interface ScrollAnchorSpacerProps extends SpacerProps {
-  readonly name: string;
-}
-function ScrollAnchorSpacer(props: ScrollAnchorSpacerProps) {
-  return (
-    <ScrollAnchorElement name={props.name}>
-      <Spacer {...props} />
-    </ScrollAnchorElement>
-  );
 }
 
 function FilterButton(props: FlexProps) {
@@ -379,15 +355,6 @@ function HomePage() {
     EnrollMethod[]
   >(searchFilters.enroll_method);
 
-  const bg = useColorModeValue("white", "black");
-
-  const scroll_config = {
-    duration: 1000,
-    delay: 50,
-    smooth: true,
-    offset: -60,
-  };
-
   return (
     <>
       <Head>
@@ -397,7 +364,13 @@ function HomePage() {
           content="首頁 | NTUCourse Neo，全新的臺大選課網站。"
         />
       </Head>
-      <Box maxW="screen-md" mx="auto" overflow="visible" mb="-15px" bg={bg}>
+      <Box
+        maxW="screen-md"
+        mx="auto"
+        overflow="visible"
+        mb="-15px"
+        bg={"white"}
+      >
         <NewRegisterModal
           isOpen={isOpen}
           onClose={onClose}
@@ -917,276 +890,6 @@ function HomePage() {
                 </Text>
               </Checkbox>
             </Flex>
-          </Flex>
-          <Button
-            variant="ghost"
-            size="lg"
-            mt={{ base: "8", md: "32" }}
-            color={useColorModeValue("heading.light", "heading.dark")}
-            onClick={() => scroller.scrollTo("card1", scroll_config)}
-            leftIcon={<FaArrowDown />}
-          >
-            Why Neo?
-          </Button>
-          <ScrollAnchorSpacer my={5} name="card1" />
-          <HomeCard
-            title="搜尋篩選，更快更準。 🚀"
-            desc={[
-              "試著在空堂塞入課程，或者在找尋系上的必修嗎？",
-              "我們的篩選功能，提供時間、系所與類別等條件複合篩選，不怕你找不到課，只怕你難以抉擇！",
-            ]}
-            img="/img/home_cards/search.svg"
-            bg={useColorModeValue("white", "black")}
-          />
-          <Spacer my={5} />
-          <Button
-            variant="ghost"
-            size="lg"
-            color={useColorModeValue("heading.light", "heading.dark")}
-            onClick={() => scroller.scrollTo("card2", scroll_config)}
-            leftIcon={<FaArrowDown />}
-          >
-            受夠一直切分頁了嗎？
-          </Button>
-          <ScrollAnchorSpacer my={5} name="card2" />
-          <HomeCard
-            title="並列互動式課表，選課更直覺。"
-            desc={[
-              "這堂課是第幾節上課？會不會卡到必修？",
-              "互動式課表讓時間不再只是簡單的數字，而是在課表中即時顯示。讓你更直覺地看到課程時間與你的規劃。",
-            ]}
-            img="/img/home_cards/course_table.svg"
-            bg={useColorModeValue("white", "black")}
-            imgAtLeft={true}
-          />
-          <Spacer my={5} />
-          <Button
-            variant="ghost"
-            size="lg"
-            color={useColorModeValue("heading.light", "heading.dark")}
-            onClick={() => scroller.scrollTo("card3", scroll_config)}
-            leftIcon={<FaArrowDown />}
-          >
-            小孩子才做選擇
-          </Button>
-          <ScrollAnchorSpacer my={5} name="card3" />
-          <HomeCard
-            title="我全都要。不怕衝堂，順序輕鬆排。"
-            desc={[
-              "選課好貪心，課表變得超級無敵長...",
-              "我們顛覆以往的線上課表模式，不只可衝堂加課，更能分別調整衝堂課程的順序偏好！",
-              "你只需要好好挑選適合的課程，剩下的交給我們。👌",
-            ]}
-            img="/img/home_cards/selection.svg"
-            bg={useColorModeValue("white", "black")}
-          />
-          <Spacer my={5} />
-          <Button
-            variant="ghost"
-            size="lg"
-            color={useColorModeValue("heading.light", "heading.dark")}
-            onClick={() => scroller.scrollTo("card4", scroll_config)}
-            leftIcon={<FaArrowDown />}
-          >
-            喬志願序好麻煩？
-          </Button>
-          <ScrollAnchorSpacer my={5} name="card4" />
-          <HomeCard
-            title="一鍵加入課程網，志願序一目瞭然。"
-            desc={[
-              "還在埋頭研究課程志願的先後順序嗎？",
-              "調整衝堂及全部課程的志願序後，點擊將課程直接加入課程網，同時告別瘋狂彈出的預選課程頁面。別忘了，你還能參考我們顯示的志願序數字直接填入選課系統。",
-              "就是這麼簡單，一塊蛋糕。 🍰",
-            ]}
-            img="/img/home_cards/sync.svg"
-            bg={useColorModeValue("white", "black")}
-            imgAtLeft={true}
-          />
-          <Spacer my={5} />
-          <Button
-            variant="ghost"
-            size="lg"
-            color={useColorModeValue("heading.light", "heading.dark")}
-            onClick={() => scroller.scrollTo("card5", scroll_config)}
-            leftIcon={<FaArrowDown />}
-          >
-            課程資訊一把抓
-          </Button>
-          <ScrollAnchorSpacer my={5} name="card5" />
-          <HomeCard
-            title="站在巨人的肩膀上。"
-            desc={[
-              "還在開一堆分頁，到處找散落的課程資訊嗎？",
-              "從選課人數、課程大綱、加簽資訊*，甚至於課程評價跟考古題**，我們的課程資訊頁面集合了官方與非官方的資料，一手統整所有你需要的選課資訊。",
-              "讓你輕鬆站在巨人的肩膀上選課。",
-            ]}
-            img="/img/home_cards/dashboard.svg"
-            bg={useColorModeValue("white", "black")}
-          />
-          <Flex w="100%" justifyContent="start" alignItems="center" px="10vw">
-            <Text
-              fontSize="xs"
-              fontWeight="500"
-              color={useColorModeValue("gray.300", "gray.600")}
-            >
-              * 加簽資訊為社群回報資訊，僅供參考，實際加簽情況可能會有所不同。
-              <br />
-              ** 資訊來自 PTT NTUCourse, PTT NTUExam ，
-              內容僅供參考且不代表本站立場。
-            </Text>
-          </Flex>
-          <Flex
-            w="100vw"
-            bg="gray.700"
-            px={{ base: "8", md: "16", lg: "32" }}
-            py="16"
-            mt="16"
-            flexDirection={{ base: "column", lg: "row" }}
-            justifyContent="space-between"
-            alignItems="center"
-            css={{ gap: "2rem" }}
-          >
-            <Flex
-              w={{ base: "100%", lg: "70%" }}
-              flexDirection={{ base: "column", md: "row" }}
-              align={"center"}
-            >
-              <Icon mx="8" mb="4" as={FaGithub} boxSize="16" color="white" />
-              <Flex
-                flexDirection="column"
-                align={{ base: "center", md: "start" }}
-                textAlign={{ base: "center", md: "start" }}
-              >
-                <Text fontSize="4xl" color="gray.100" fontWeight="800">
-                  動手參與開發
-                </Text>
-                <Text mt="2" fontSize="lg" color="gray.100" fontWeight="500">
-                  不管是 Issue 、 PR 或甚至加入我們 ，歡迎一起來讓 NTUCourse Neo
-                  變得更加完美。
-                </Text>
-              </Flex>
-            </Flex>
-            <Flex
-              justify={{ base: "center", md: "start" }}
-              flexDirection={{ base: "column", md: "row" }}
-              flexWrap="wrap"
-            >
-              <Button
-                m={2}
-                variant="solid"
-                size="lg"
-                onClick={() => {
-                  window.open("https://github.com/NTUCourse-Neo/");
-                  reportEvent("home_page", "click", "github_repo");
-                }}
-                leftIcon={<FaGithub />}
-              >
-                GitHub
-              </Button>
-              <Button
-                m={2}
-                variant="outline"
-                size="lg"
-                color="gray.200"
-                borderColor="gray.500"
-                onClick={() => {
-                  window.open(
-                    "https://github.com/NTUCourse-Neo/ncn-frontend/issues/new/choose"
-                  );
-                  reportEvent("home_page", "click", "github_issue");
-                }}
-              >
-                功能建議
-              </Button>
-            </Flex>
-          </Flex>
-          <Flex
-            w="100vw"
-            bg="#5865F2"
-            px={{ base: "8", md: "16", lg: "32" }}
-            py="16"
-            flexDirection={{ base: "column", lg: "row" }}
-            justifyContent="space-between"
-            alignItems="center"
-            flexWrap="wrap"
-            css={{ gap: "2rem" }}
-          >
-            <Flex
-              w={{ base: "100%", lg: "65%" }}
-              flexDirection={{ base: "column", md: "row" }}
-              align={"center"}
-            >
-              <Icon mx="8" mb="4" as={DiscordIcon} boxSize="16" color="white" />
-              <Flex
-                flexDirection="column"
-                align={{ base: "center", md: "start" }}
-                textAlign={{ base: "center", md: "start" }}
-              >
-                <Text fontSize="4xl" color="gray.100" fontWeight="800">
-                  加入社群
-                </Text>
-                <Text mt="2" fontSize="lg" color="gray.100" fontWeight="500">
-                  一起進來聊聊天、回報問題或給予功能建議，都超讚的啦！
-                </Text>
-              </Flex>
-            </Flex>
-            <Flex
-              justify={{ base: "center", md: "start" }}
-              flexDirection={{ base: "column", md: "row" }}
-            >
-              <Button
-                variant={"solid"}
-                size="lg"
-                onClick={() => {
-                  window.open("https://discord.gg/M7NrenYEbS");
-                  reportEvent("home_page", "click", "discord");
-                }}
-                bg={"white"}
-                color={"#5865F2"}
-                leftIcon={<DiscordIcon />}
-              >
-                Join #NTUCourse-Neo
-              </Button>
-            </Flex>
-          </Flex>
-          <Flex
-            w="100vw"
-            px={{ base: "8", md: "16", lg: "32" }}
-            pt="8"
-            justifyContent="space-between"
-            alignItems="center"
-            flexDirection={{ base: "column", lg: "row" }}
-            css={{ gap: "2rem" }}
-          >
-            <Flex
-              flexDirection="column"
-              align={{ base: "center", lg: "start" }}
-              textAlign={{ base: "center", md: "start" }}
-            >
-              <Text
-                py={2}
-                fontSize={{ base: "3xl", md: "4xl" }}
-                color={useColorModeValue("text.light", "text.dark")}
-                fontWeight="800"
-              >
-                現在就開始體驗新世代的選課吧。
-              </Text>
-              <Text fontSize="lg" color="teal.500" fontWeight="500">
-                由學生開發、維運，最懂你的選課網站。
-              </Text>
-              <Link href="course">
-                <Button mt="8" variant="solid" size="lg" colorScheme="teal">
-                  開始使用
-                </Button>
-              </Link>
-            </Flex>
-            <Image
-              alt=""
-              src={`/img/home_footer.svg`}
-              height="256px"
-              width="256px"
-              pointerEvents="none"
-            />
           </Flex>
         </Flex>
       </Box>
