657e6a63270f136f589579635ab485e94b8d17cb
swh00tw <a6140000@gmail.com>
2022-10-21 12:47:38 +0800

    finish right panel

--- src/components/UserCoursePanel.tsx
+++ src/components/UserCoursePanel.tsx
@@ -3,14 +3,87 @@ import { useState } from "react";
 import { FiCalendar, FiHeart } from "react-icons/fi";
 import { FaChevronDown } from "react-icons/fa";
 import type { IconType } from "react-icons";
+import { useUser } from "@auth0/nextjs-auth0";
+import useNeoLocalStorage from "hooks/useNeoLocalStorage";
+import useCourseTable from "@/hooks/useCourseTable";
+import useUserInfo from "@/hooks/useUserInfo";
+import { Course } from "@/types/course";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { parseCourseTimeLocation } from "@/utils/parseCourseSchedule";
+
+interface CourseInfoCardProps {
+  readonly course: Course;
+}
+function CourseInfoCard(props: CourseInfoCardProps) {
+  const { course } = props;
+  const timeLocation = parseCourseTimeLocation(course.schedules);
+  return (
+    <Flex
+      w="100%"
+      sx={{
+        shadow: "0px 1px 2px rgba(112, 123, 139, 0.25)",
+      }}
+      flexDirection="column"
+      p={4}
+      gap={1}
+    >
+      <Flex w="100%">
+        <Flex
+          flexGrow={1}
+          sx={{
+            fontWeight: 500,
+            fontSize: "14px",
+            lineHeight: "1.6",
+            color: "#333333",
+          }}
+        >
+          {course.name}
+        </Flex>
+        <Flex>
+          <Center
+            h="24px"
+            w="24px"
+            borderRadius={"12px"}
+            cursor="pointer"
+            sx={{
+              _hover: {
+                bg: "black.300",
+              },
+            }}
+          >
+            <Icon as={BsThreeDotsVertical} boxSize="20px" color={"black"} />
+          </Center>
+        </Flex>
+      </Flex>
+      <Flex
+        sx={{
+          fontSize: "12px",
+          lineHeight: "1.6",
+          color: "#6f6f6f",
+        }}
+      >
+        {course?.teacher}
+      </Flex>
+      <Flex
+        sx={{
+          fontSize: "12px",
+          lineHeight: "1.6",
+          color: "#333333",
+        }}
+      >
+        {timeLocation.map((pair) => pair.time).join(" / ")}
+      </Flex>
+    </Flex>
+  );
+}
 
 interface PanelProps {
-  layout: string;
-  isOpen: boolean;
-  title: string;
-  onClick: () => void;
-  icon: IconType;
-  children?: React.ReactNode;
+  readonly layout: string;
+  readonly isOpen: boolean;
+  readonly title: string;
+  readonly onClick: () => void;
+  readonly icon: IconType;
+  readonly children?: React.ReactNode;
 }
 function Panel(props: PanelProps) {
   const { layout, isOpen, title, onClick, icon, children = null } = props;
@@ -76,11 +149,19 @@ function UserCoursePanel() {
   const [layout, setLayout] = useState<"default" | "course" | "favorite">(
     "default"
   );
+  const { neoLocalCourseTableKey } = useNeoLocalStorage();
+  const { user } = useUser();
+  const { userInfo } = useUserInfo(user?.sub ?? null);
+  const courseTableKey = userInfo
+    ? userInfo?.course_tables?.[0] ?? null
+    : neoLocalCourseTableKey;
+  const { courseTable } = useCourseTable(courseTableKey);
+
   return (
     <Flex flexDirection={"column"} h="100%" gap={8}>
       <Panel
         layout={layout}
-        title="已加入課程"
+        title={`已加入課程 (${courseTable?.courses.length})`}
         icon={FiCalendar}
         isOpen={layout === "default" || layout === "course"}
         onClick={() => {
@@ -91,15 +172,13 @@ function UserCoursePanel() {
           }
         }}
       >
-        {Array.from({ length: 20 }).map((_, index) => (
-          <Flex w="100%" key={index}>
-            {index}
-          </Flex>
+        {courseTable?.courses.map((course) => (
+          <CourseInfoCard key={course.id} course={course} />
         ))}
       </Panel>
       <Panel
         layout={layout}
-        title="我的收藏"
+        title={`我的收藏 (${userInfo?.favorites.length})`}
         icon={FiHeart}
         isOpen={layout === "default" || layout === "favorite"}
         onClick={() => {
@@ -110,10 +189,8 @@ function UserCoursePanel() {
           }
         }}
       >
-        {Array.from({ length: 20 }).map((_, index) => (
-          <Flex w="100%" key={index}>
-            {index}
-          </Flex>
+        {userInfo?.favorites.map((course) => (
+          <CourseInfoCard key={course.id} course={course} />
         ))}
       </Panel>
     </Flex>
