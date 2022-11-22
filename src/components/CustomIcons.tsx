43b8991d8722f997e59b869050ec18de7c57c6d1
swh00tw <a6140000@gmail.com>
2022-11-22 16:16:48 +0800

    add custom icon

--- src/components/CustomIcons.tsx
+++ src/components/CustomIcons.tsx
@@ -24,4 +24,50 @@ const SearchOutlineIcon = (props: IconProps) => (
   </Icon>
 );
 
-export { DiscordIcon, StopWatchOutlineIcon, SearchOutlineIcon };
+const CoffeeOutlineIcon = (props: IconProps) => (
+  <Icon {...props}>
+    <path
+      d="M4 20H18"
+      stroke-width="2"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M18 14H17C17 14.5523 17.4477 15 18 15V14ZM18 8L18 7C17.4477 7 17 7.44772 17 8L18 8ZM19 8V7V8ZM19 14V15V14ZM19 14L19 8L17 8L17 14H19ZM18 9H19V7L18 7L18 9ZM19 13H18V15H19V13ZM21 11C21 12.1046 20.1046 13 19 13V15C21.2091 15 23 13.2091 23 11H21ZM19 9C20.1046 9 21 9.89543 21 11H23C23 8.79086 21.2091 7 19 7V9Z"
+      fill="currentColor"
+    />
+    <path
+      d="M4 8V7C3.44772 7 3 7.44772 3 8H4ZM18 8H19C19 7.44772 18.5523 7 18 7V8ZM4 9H18V7H4V9ZM17 8V13H19V8H17ZM5 13V8H3V13H5ZM11 19C7.68629 19 5 16.3137 5 13H3C3 17.4183 6.58172 21 11 21V19ZM17 13C17 16.3137 14.3137 19 11 19V21C15.4183 21 19 17.4183 19 13H17Z"
+      fill="currentColor"
+    />
+    <path
+      stroke-width="1.5"
+      d="M15 3L14 5"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      stroke-width="1.5"
+      d="M12 3L11 5"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      stroke-width="1.5"
+      d="M9 3L8 5"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </Icon>
+);
+
+export {
+  DiscordIcon,
+  StopWatchOutlineIcon,
+  SearchOutlineIcon,
+  CoffeeOutlineIcon,
+};
